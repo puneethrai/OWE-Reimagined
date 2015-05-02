d2dfcab9ad7aebf519a8c3f62357423936870a72
Puneeth Rai <npuneethrai@yahoo.co.in>
2015-05-02 18:36:37 +0530

    remove old test script and modules update

--- OWE/www/app/modules/modules.js
+++ OWE/www/app/modules/modules.js
@@ -1,13 +1,15 @@
-define(function(require) {
+/*globals define*/
+define(function (require) {
 
-	/* 
-	 * Return an array containing all modules classes that needs to be initiated.
-	 * We use the 'require' function of requirejs to get the relevant module context classes.
-	 * This could be done of course, by passing those scripts as dependencies to 
-	 * the 'define' function above. But following model is a bit simpler to read.
-	 */
-	return [
-		require('./baseModule/module'),
-	];
-
+    /* 
+     * Return an array containing all modules classes that needs to be initiated.
+     * We use the 'require' function of requirejs to get the relevant module context classes.
+     * This could be done of course, by passing those scripts as dependencies to 
+     * the 'define' function above. But following model is a bit simpler to read.
+     */
+    return [
+        require('./baseModule/module'),
+        require('./friendsModule/module'),
+        require('./transactionModule/module')
+    ];
 });
