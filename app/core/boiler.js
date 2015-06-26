3d302e10fc9b60384577fcf4be72feb1e968fada
Puneeth Rai <npuneethrai@yahoo.co.in>
2015-06-27 00:18:12 +0530

    renaming helper

--- app/core/boiler.js
+++ app/core/boiler.js
@@ -18,6 +18,6 @@ define(function (require) {
     **/
     return {
         Context : require("./app/core/context"),
-        Helpers : require("./app/core/helpers/_helpers_")
+        Helpers : require("./app/core/helpers/helpers")
     };
 });
